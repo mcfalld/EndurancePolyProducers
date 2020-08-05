2d53e24de7d38b83724a3a1e5be1f411217854f5
Derrick <mcfall.derrick@gmail.com>
2020-08-05 12:59:51 -0500

    mobile navbar

--- navbar.php
+++ navbar.php
@@ -11,10 +11,17 @@ function active($currect_page){
     <div class="nav-wrapper deep-orange">
         <a href="index.php" class="brand-logo center"><img src="./images/Endurance5.png" width="200">
         </a>
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul class="left hide-on-med-and-down">
             <li class="<?php active('index.php');?>"><a href="index.php">Home</a></li>
             <li class="<?php active('contact.php');?>"><a href="contact.php">Contact Us</a></li>
-            <!-- <li class="active"><a href="collapsible.html">JavaScript</a></li> -->
         </ul>
     </div>
+
+    <ul class="side-nav" id="mobile-demo">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+  </ul>
+
+
 </nav>
