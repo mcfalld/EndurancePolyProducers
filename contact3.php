7698e08f32fa4c96531f33d8f185c8c5f94a9fb0
Derrick <mcfall.derrick@gmail.com>
2021-05-05 22:21:29 -0500

    update contact form, unhide it

---
+++ contact3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+<?php include ('head.php'); ?>
+
+<body>
+    <header>
+<!-- navbar -->
+<?php include_once ('navbar.php'); ?>
+</header>
+<main style="@import url('https://fonts.googleapis.com/css2?family=Work+Sans&display=swap'); background: url('./images/background.jpg')" class="white-text">
+
+
+<?php require('beautiful-contact-form-html\formpage.html'); ?>
+
+
+</main>
+<footer>
+<?php include_once ('footer.php'); ?>
+</footer>
+    <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+
+</html>
